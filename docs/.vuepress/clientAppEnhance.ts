206a99c09e58f8be5eee961e8f43fd9fc1d37610
Mostafa <mostafa.sedaghat@gmail.com>
2022-09-13 01:25:42 +0800

    Removing vuepress docs

--- docs/.vuepress/clientAppEnhance.ts
+++
@@ -1,7 +0,0 @@
-import { defineClientAppEnhance } from '@vuepress/client'
-import hexdump from './components/hexdump.vue'
-
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-  app.component('hexdump', hexdump)
-})
-
