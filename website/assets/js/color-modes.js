f2cdc850c9ff29e9af152436a043e89c35ebc727
Mostafa <mostafa.sedaghat@gmail.com>
2024-01-31 03:20:47 +0800

    fixing dark mode issue on the first page

--- website/assets/js/color-modes.js
+++ website/assets/js/color-modes.js
@@ -16,7 +16,7 @@
       return storedTheme
     }
 
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'dark'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
 
   const setTheme = theme => {
